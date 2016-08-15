72ac2754a5df6a19948b910183e4f9303af46195
Vaishaal Shankar <vaishaal@berkeley.edu>
2016-08-15 14:58:29 -0700

    Fixed bug in folds * Two functions added one for deterministic train test split asper competition standards * Other does random cv for our own edification

--- src/main/scala/net/akmorrow13/endive/utils/Utils.scala
+++ src/main/scala/net/akmorrow13/endive/utils/Utils.scala
@@ -1,52 +1,38 @@
 package net.akmorrow13.endive.utils
 
 import org.apache.spark.rdd.RDD
-import Crossable.trav2Crossable
+import scala.util.Random
 
 object EndiveUtils {
 
 /* Generate folds RDD */
-def generateFoldsRDD(allData:RDD[LabeledWindow], cellTypesPerFold: Int = 1, chromosomesPerFold: Int = 11, sampleFreq: Double = 0.001 ) = {
+def generateFoldsRDD(allData:RDD[LabeledWindow], numHeldOutCellTypes: Int = 1, numHeldOutChromosomes: Int = 3, numFolds: Int = 10, sampleFreq: Double = 0.001, randomSeed:Int = 0) = {
+
+    @transient
+    val r = new Random(randomSeed)
+
+    val sampledData = allData.sample(false, sampleFreq)
+
+    val cellTypesChromosomes:Set[(String, String)] = sampledData.map(x => (x.win.getRegion.referenceName, x.win.cellType)).countByValue().keys.toSet
 
     /* this will work with exponentially high probability */
-    val cellTypes:Iterable[String] = allData.sample(false, sampleFreq).map(x => (x.win.cellType)).countByValue().keys
-
-    val cellTypesMap = cellTypes.zipWithIndex.toMap
+    val cellTypes:Iterable[String] = sampledData.map(x => (x.win.cellType)).countByValue().keys
 
     /* this will work with exponentially high probability */
-    val chromosomes:Iterable[String] = allData.sample(false, sampleFreq).map(x => (x.win.getRegion.referenceName)).countByValue().keys
-
-    val chromosomesMap = chromosomes.zipWithIndex.toMap
-
-    val numChromesomeFolds: Int = chromosomes.size/chromosomesPerFold
-    val numCellTypeFolds : Int = cellTypes.size/chromosomesPerFold
-
-    val numFolds = numChromesomeFolds * numCellTypeFolds
-
-    val cellTypesChromesomes:Set[(String, String)] = (chromosomes × cellTypes).toSet
+    val chromosomes:Iterable[String] = sampledData.map(x => (x.win.getRegion.referenceName)).countByValue().keys
 
     for (i <- (0 until numFolds)) yield
         {
-        val trainSet = cellTypesChromesomes.filter { x =>
-                                chromosomesMap(x._1) % numFolds != i ||
-                                cellTypesMap(x._2) % numFolds != i
-                                }
-
-        val testSet = cellTypesChromesomes.filter { x =>
-                                chromosomesMap(x._1) % numFolds == i &&
-                                cellTypesMap(x._2) % numFolds == i
-                                }
-
-        assert(trainSet.intersect(testSet).size == 0)
-
-        val train = allData.filter { window =>
-                                     trainSet.contains((window.win.getRegion.referenceName,  window.win.cellType))
-                                    }
-        val test = allData.filter { window =>
-                                     testSet.contains((window.win.getRegion.referenceName,  window.win.cellType))
-                                    }
-        (train, test)
+        val holdOutCellTypes = r.shuffle(cellTypes).take(numHeldOutCellTypes).toSet
+        val holdOutChromosomes = r.shuffle(chromosomes).take(numHeldOutChromosomes).toSet
+        generateTrainTestSplit(allData, holdOutCellTypes, holdOutChromosomes)
         }
 }
 
+def generateTrainTestSplit(allData: RDD[LabeledWindow], holdOutCellTypes: Set[String],
+ holdOutChromosomes: Set[String]) = {
+        val train = allData.filter { window => !holdOutChromosomes.contains(window.win.getRegion.referenceName) && !holdOutCellTypes.contains(window.win.cellType) }.cache()
+        val test = allData.filter { window => holdOutChromosomes.contains(window.win.getRegion.referenceName) && holdOutCellTypes.contains(window.win.cellType) }.cache()
+        (train, test)
+ }
 }
